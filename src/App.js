4a6ff4f79dc9a37b6ea0d0cf6e94c0919bcab70f
Archana7046 <84974842+Archana7046@users.noreply.github.com>
2021-07-30 12:00:44 +0530

    first react project

---
+++ src/App.js
@@ -0,0 +1,28 @@
+//import React from 'react'
+
+/*import React from "react";
+
+const App = () => {
+  return(
+  <>
+  <FirstName/>
+  <h1 className="myclassname"> hello world  </h1>
+  <p>Awesome</p>
+  <img src="" alt ="" />
+  </>
+  );
+}
+
+const FirstName = () =>{
+  return <h1> Bytecode clsses</h1>
+}
+export default App;
+*/
+
+import React from 'react'
+import Restaurant from "./component/Basics/Restaurant"
+const App = () => {
+  return <Restaurant/>; 
+};
+
+export default App;
